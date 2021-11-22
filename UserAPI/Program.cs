fccdd558c5bd39fd058ba580cba0cd9bab019add
szymon <kusnierek.szymon@gmail.com>
2021-11-23 00:02:41 +0100

    Walking Skeleton

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World! \nI'm Get Method!");
+app.MapPut("/", () => "Hello World! \nI'm Put Method!");
+app.MapDelete("/", () => "Hello World! \nI'm Delete Method!");
+app.MapPost("/", () => "Hello World! \nI'm Post Method!");
 
 app.Run();
